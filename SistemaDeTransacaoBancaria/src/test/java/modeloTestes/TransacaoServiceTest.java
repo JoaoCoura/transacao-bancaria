84177fe5e8d5517151b96975d159a400dccb2ab7
Joao Pedro Basso Coura <jpbcoura@gmail.com>
2025-05-28 07:51:10 -0300

    Melhoria

--- SistemaDeTransacaoBancaria/src/test/java/modeloTestes/TransacaoServiceTest.java
+++ SistemaDeTransacaoBancaria/src/test/java/modeloTestes/TransacaoServiceTest.java
@@ -14,6 +14,7 @@ import static org.example.util.MensagensTransacao.*;
 public class TransacaoServiceTest {
 
     private static final Faker faker = new Faker();
+
     @Test
     public void depositar_ContaAtivaESemSaldo_DeveAumentarSaldoDaContaDestinoRegistrarTransacaoEIncrementarPontos()
     {
